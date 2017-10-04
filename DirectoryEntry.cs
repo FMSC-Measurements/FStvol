b4c26dc7579d8cfe1e2da4831d3b9ff1e53f38d9
rhiltonFS <rhilton@fs.fed.us>
2017-10-04 11:37:28 -0600

    Android Addon Tree Volume
    Android Addon Tree Volume for Sales Admins

---
+++ DirectoryEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FS_AddVolAndroid
+{
+	/// <summary>
+	/// Entity class that represents an directory entry.
+	/// </summary>
+	public class DirectoryEntry
+	{
+		public String fileName;
+		public String mimeType;
+	}
+
+}
+
